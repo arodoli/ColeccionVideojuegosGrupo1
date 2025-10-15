09d12ee6cf256e93ee8fee86894bdf4ccfa722f6
Alberto Carmona <carmonaolleralberto@gmail.com>
2025-10-15 14:45:53 +0200

    Conexion a la base de datos

--- src/ConexionDB.java
+++ src/ConexionDB.java
@@ -16,15 +16,15 @@ public class ConexionDB {
     // URL de conexión a la base de datos.
     // Reemplaza 'localhost:3306' si tu servidor MySQL corre en otro puerto o host.
     // 'videojuegos_db' es el nombre de la base de datos creada con el script SQL.
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/videojuegos_db?useSSL=false&serverTimezone=UTC";
+    private static final String DB_URL = "jdbc:mariaDB";
 
     // Usuario de la base de datos.
     // Reemplaza "tu_usuario" por tu nombre de usuario de MySQL (p.ej., "root").
-    private static final String USER = "tu_usuario";
+    private static final String USER = "root";
 
     // Contraseña de la base de datos.
     // Reemplaza "tu_contraseña" por tu contraseña de MySQL.
-    private static final String PASS = "tu_contraseña";
+    private static final String PASS = "root";
 
     /**
      * Establece y devuelve una conexión con la base de datos.
