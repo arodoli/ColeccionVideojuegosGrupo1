92bd80cebf1c43ff6e3719c36fdfafd566437e92
pablomil2000 <martinlopezpablo@gmail.com>
2025-10-20 10:57:25 +0200

    Javadoc

--- src/GestorVideojuegos.java
+++ src/GestorVideojuegos.java
@@ -1,27 +1,61 @@
 public class GestorVideojuegos {
     Videojuego[] lista = new Videojuego[];
 
+
+    /**
+     *
+     * @param lista
+     */
     public GestorVideojuegos(Videojuego[] lista) {
         this.lista = lista;
     }
 
-    public boolean addGame(){
+
+    /**
+     *
+     * @param titulo String
+     * @param genero String
+     * @param plataforma String
+     * @param anio int
+     * @param valoracion int (1-10)
+     * @return boolean
+     */
+    public boolean addGame(String titulo, String genero, String plataforma, int anio, int valoracion) {
 //        Implementacion
     }
 
-    public boolean removeGame(){
+    /**
+     *
+     * @param id juego a eliminar
+     * @return boolean
+     */
+    public boolean removeGame(int id){
 //        Implementacion
     }
 
-
-    public Videojuego[] search(int campoBuscar) {
+    /**
+     *
+     * @param campoBuscar [1: Titulo, 2: Plataforma, 3: Genero]
+     * @param valor: valor por el que buscamos
+     * @return List[] de juegos
+     */
+    public Videojuego[] search(int campoBuscar, String valor) {
 //        Implementacion
     }
 
+    /**
+     *
+     * @return List[] de juegos
+     */
     public Videojuego[] getLista() {
 //        Implementacion
     }
 
+    /**
+     *
+     * @param count maximo de juegos que se van a mostrar
+     * @return List[] de juegos
+     */
     public Videojuego[] bestScore(int count){
 //        Implementacion
     }
