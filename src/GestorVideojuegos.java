8af9e87140691c85689fb6a1dc07a911057b61b7
pablomil2000 <martinlopezpablo@gmail.com>
2025-10-20 10:53:46 +0200

    Creacion clase GestorVideojuegos

---
+++ src/GestorVideojuegos.java
@@ -0,0 +1,28 @@
+public class GestorVideojuegos {
+    Videojuego[] lista = new Videojuego[];
+
+    public GestorVideojuegos(Videojuego[] lista) {
+        this.lista = lista;
+    }
+
+    public boolean addGame(){
+//        Implementacion
+    }
+
+    public boolean removeGame(){
+//        Implementacion
+    }
+
+
+    public Videojuego[] search(int campoBuscar) {
+//        Implementacion
+    }
+
+    public Videojuego[] getLista() {
+//        Implementacion
+    }
+
+    public Videojuego[] bestScore(int count){
+//        Implementacion
+    }
+}
